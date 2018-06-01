a3cb6cf6be15340f0f904940c8c3562609faa49c
DESKTOP-8G69KAN\root <nkc17.yuya.hayase@gmail.com>
2018-06-01 14:49:57 +0900

    エネミー等

---
+++ ShadowMove/Assets/Script/Hayase/hEnemy.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+abstract class hEnemy : MonoBehaviour {
+
+    /// <summary>
+    /// エネミーのステータス
+    /// </summary>
+    public enum Status
+    {
+        Wait,
+        Attack,
+        Move
+    }
+
+    /// <summary>
+    /// エネミーの移動方向
+    /// </summary>
+    public enum CharacterMoveDirection
+    {
+        Top,
+        TopRight,
+        Right,
+        BottomRight,
+        Bottom,
+        BottomLeft,
+        Left,
+        TopLeft
+    }
+
+    // 座標記憶
+    public Vector2 pos;
+
+    // 抽象化キャラクターのステータス
+    public abstract void CharacterStatus(Status status);
+    
+    // 移動
+    public Vector2 CharacterMove(CharacterMoveDirection cmd)
+    {
+        float dx = 0, dy = 0;
+
+        switch (cmd)
+        {
+            case CharacterMoveDirection.Top:
+                dy = 1;
+                break;
+            case CharacterMoveDirection.Right:
+                dx = 1;
+                break;
+            case CharacterMoveDirection.Bottom:
+                dy = -1;
+                break;
+            case CharacterMoveDirection.Left:
+                dx = -1;
+                break;
+            case CharacterMoveDirection.TopRight:
+                CharacterMove(CharacterMoveDirection.Top);
+                CharacterMove(CharacterMoveDirection.Right);
+                break;
+            case CharacterMoveDirection.TopLeft:
+                CharacterMove(CharacterMoveDirection.Top);
+                CharacterMove(CharacterMoveDirection.Left);
+                break;
+            case CharacterMoveDirection.BottomRight:
+                CharacterMove(CharacterMoveDirection.Bottom);
+                CharacterMove(CharacterMoveDirection.Right);
+                break;
+            case CharacterMoveDirection.BottomLeft:
+                CharacterMove(CharacterMoveDirection.Bottom);
+                CharacterMove(CharacterMoveDirection.Left);
+                break;
+        }
+
+        pos = new Vector2(dx / 30.0f, dy / 30.0f);
+        return pos;
+    }
+}
---
+++ ShadowMove/Assets/Script/Hayase/hEnemy1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+class hEnemy1 : hEnemy {
+
+    // 攻撃しているかどうか
+    static bool isAttacking = false;
+
+    // Use this for initialization
+	void Start () {
+        pos = new Vector2(transform.position.x, transform.position.y);
+	}
+	
+    // Update
+    private void FixedUpdate()
+    {
+        CharacterStatus(Status.Move);
+    }
+
+    // キャラクターのステータス
+    public override void CharacterStatus(Status status)
+    {
+        switch (status)
+        {
+            case Status.Attack:
+                setAttacking = true;
+                break;
+            case Status.Move:
+                pos += CharacterMove(CharacterMoveDirection.Left);
+                transform.position = pos;
+                break;
+            case Status.Wait:
+                break;
+        }
+    }
+
+    private static bool setAttacking
+    {
+        set
+        {
+            isAttacking = value;
+        }
+    }
+
+    public static bool getAttacking
+    {
+        get
+        {
+            return isAttacking;
+        }
+    }
+}
