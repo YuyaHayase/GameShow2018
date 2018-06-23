b5aba53307c42c6e4944ce07b0942dc3a91ed77e
DESKTOP-8G69KAN\root <nkc17.yuya.hayase@gmail.com>
2018-06-24 04:42:13 +0900

    Title

--- ShadowMove/Assets/Script/Hayase/hTitleNeedle.cs
+++ ShadowMove/Assets/Script/Hayase/hTitleNeedle.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class hTitleNeedle : MonoBehaviour {
 
@@ -35,19 +36,31 @@ public class hTitleNeedle : MonoBehaviour {
 
     [SerializeField]
     GameObject TitleLogo;
+    GameObject tl;      // タイトルロゴの影
+
+    [SerializeField, Header("読み込むシーンの名前")]
+    string[] SceneName = new string[] {"Main", "Config" };
 
     // 背景色
-    Color[] SkyColor = {new Color(0.6f, 1, 1), new Color(0.8f,0.4f,0), new Color(0.9f,0.9f,0f) };
+    Color[] SkyColor;
 
     // タイトル影、ポジション係数
     [SerializeField, Header("影のy移動")]
-    float yTitleMove = 1.0f;
+    float yTitleMove = 0.4f;
 
     void Start()
     {
-        GameObject ts = GameObject.Find("TitleLogo") as GameObject;
-        ts.GetComponent<Renderer>().material.color = Color.black;
-        Instantiate(ts);
+        // カメラの背景色の設定
+        SkyColor = new Color[] { IntToNorm(153, 255, 255), IntToNorm(204, 102, 0), IntToNorm(230, 230, 0) };
+
+        // タイトルロゴが入れられてたら
+        if(null != TitleLogo)
+        {
+            tl = Instantiate(TitleLogo) as GameObject;
+            tl.GetComponent<Renderer>().material.color = IntToNorm(130, 130, 130);
+            tl.GetComponent<Renderer>().sortingOrder = -1;
+        }
+
     }
 
     // Update is called once per frame
@@ -65,6 +78,16 @@ public class hTitleNeedle : MonoBehaviour {
         if (t < 0.025f) t = 0;
         if (t > 0.975f) t = 1;
 
+        // 矢印の選択
+        if (Input.GetKeyUp(KeyCode.Return))
+        {
+            if (t == 1) Application.Quit();
+            else if (t == 0) SceneLoad(SceneName[0]);
+            else SceneLoad(SceneName[1]);
+        }
+
+        // タイトルロゴの影の移動
+        tl.transform.position = TitleLogo.transform.position + new Vector3(0.4f - (t * yTitleMove), -0.2f);
 
         // 文字の透過
         Rend(GameStart, (1 - t));
@@ -74,16 +97,31 @@ public class hTitleNeedle : MonoBehaviour {
         Rend(Quit, t);
         Rend(QuitJp, t);
 
+        // カメラの背景の色の変更
         Camera.main.backgroundColor = (1 - t) * SkyColor[0] + t * SkyColor[1];
 
         // 実回転
         transform.rotation = Quaternion.AngleAxis((1-t) * minRotate + t * maxRotate, Vector3.forward);
 	}
 
+    // オブジェクトの透明度を変えます
    private void Rend(GameObject g, float alpha)
     {
         Color objColor = g.GetComponent<Renderer>().material.color;
         g.GetComponent<Renderer>().material.color = new Color(objColor.r, objColor.g, objColor.b, alpha);
+    }
+
+    // 255 -> 1 // 0 - 255 を 0 - 1 にします
+    private Color IntToNorm(int r,int  g, int b)
+    {
+        Color rtnColor = new Color(r / 255.0f, g / 255.0f, b / 255.0f);
+        return rtnColor;
+    }
+
+    private void SceneLoad(string name)
+    {
+        Debug.Log(name);
+        //SceneManager.LoadScene(name);
     }
 }
 
