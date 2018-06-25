0de0905ba061e5f1a4e8babdb4e98e82fae1ae1d
yosida.y <nkc17.yuuhi.yosida@gmail.com>
2018-06-25 09:46:47 +0900

    カメラとかボスとか

---
+++ ShadowMove/Assets/Script/yosida/yCameraMove2.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class yCameraMove2 : MonoBehaviour {
+
+    //--------------
+    //|  使わない  |
+    //--------------
+    GameObject player;
+    GameObject shodow;
+
+    [SerializeField,Header("カメラが追従できる最小値")]
+    float min;
+    [SerializeField,Header("")]
+    float max;
+
+    [SerializeField, Header("カメラ調整")]
+    float y;
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.Find("player");
+        shodow = GameObject.Find("shodow");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector2 playerSize = player.GetComponent<SpriteRenderer>().bounds.size;
+
+        //カメラの追従
+        transform.position = new Vector3(shodow.transform.position.x,
+                                         shodow.transform.position.y,
+                                         transform.position.z);
+
+        //縦のサイズ
+        float px = player.transform.position.x - shodow.transform.position.x;
+        float py = player.transform.position.y - shodow.transform.position.y;
+
+        if (Mathf.Abs(py) >= 4.5f)
+            Camera.main.orthographicSize = Mathf.Abs(py) + (player.GetComponent<SpriteRenderer>().bounds.size.y / 2);
+
+
+        //横のサイズ
+
+        //print(Mathf.Abs(px - (playerSize.x / 2)));
+
+        //左
+        if (Mathf.Abs(px) >= 8)
+            //          Camera.main.orthographicSize = Mathf.Abs(px) + (playerSize.x / 2);
+            Camera.main.orthographicSize = Mathf.Abs(px) + (player.GetComponent<SpriteRenderer>().bounds.size.x / 2);
+
+        //if (player.transform.position.x - (playerSize.x / 2) <= CameraTopLeft().x)
+        //{
+        //    print("aaaa");
+        //    Camera.main.orthographicSize += 0.1f;
+        //}
+        //else
+        //{
+        //    Camera.main.orthographicSize -= 0.1f;
+        //}
+
+        //if (player.transform.position.x + (playerSize.x / 2) >= CameraBottomRight().x)
+        //{
+        //    print("atari");
+        //    Camera.main.orthographicSize -= 0.1f;
+        //}
+
+
+        if (Camera.main.orthographicSize < 8)
+            Camera.main.orthographicSize = 5;
+
+        //transform.position = new Vector3
+        //    (player.transform.position.x,
+        //     player.transform.position.y + y,
+        //     transform.position.z
+        //     );
+    }
+
+    private Vector3 CameraTopLeft()
+    {
+        //カメラのワールド座標を取得
+        Vector3 cameraWorldPosLeft = Camera.main.ScreenToWorldPoint(Vector3.zero);
+
+        return cameraWorldPosLeft;
+    }
+
+    private Vector3 CameraBottomRight()
+    {
+        Vector3 cameraWorldPosRight = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width,Screen.height,0));
+
+        return cameraWorldPosRight;
+    }
+}
---
+++ ShadowMove/Assets/Script/yosida/yPlayerManager.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class yPlayerManager : MonoBehaviour {
+
+    [SerializeField, Header("画面外に出たときに表示する画像")]
+    SpriteRenderer spr;
+
+    [SerializeField, Header("Playerが移動できるx軸の最大範囲")]
+    float xMax;
+    [SerializeField, Header("Playerが移動できるx軸の最小範囲")]
+    float xMin;
+
+    bool flgOut = false;
+
+	// Use this for initialization
+	void Start () {
+        spr = Instantiate(spr);
+        spr.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //------Playerが動ける範囲制限--------
+		if(transform.position.x >= xMax)
+        {
+            transform.position = new Vector3(xMax, transform.position.y, 0);
+        }
+
+        if(transform.position.x <= xMin)
+        {
+            transform.position = new Vector3(xMin, transform.position.y, 0);
+        }
+
+
+        //-------画面外に出たら画像を出す--------
+        if (flgOut)
+        {
+            Vector3 cameraPos;
+            Vector3 cameraTopLeft = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0));
+            Vector3 cameraBottomRight = Camera.main.ScreenToWorldPoint(Vector3.zero);
+
+            float sprPosY;
+
+            //右に画像がでる
+            if (transform.position.x > Camera.main.transform.position.x)
+                cameraPos = cameraTopLeft;
+            else//左に画像がでる
+                cameraPos = cameraBottomRight;
+
+            if (transform.position.y >= cameraTopLeft.y)
+                sprPosY = cameraTopLeft.y;
+            else if (transform.position.y <= cameraBottomRight.y)
+                sprPosY = cameraBottomRight.y;
+            else
+                sprPosY = transform.position.y;
+
+            spr.transform.position = new Vector3(cameraPos.x, sprPosY, 0);
+            spr.enabled = true;
+        }
+        else
+        {
+            spr.enabled = false;
+        }
+	}
+
+    //画面外に出たとき
+    private void OnBecameInvisible()
+    {
+        flgOut = true;
+    }
+
+    //画面内に入ったとき
+    private void OnBecameVisible()
+    {
+        flgOut = false;
+    }
+
+}
