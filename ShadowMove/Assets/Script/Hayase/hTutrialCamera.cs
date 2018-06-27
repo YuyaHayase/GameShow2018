abe304c145e140d350ec0edce8db4ea53bd1240e
DESKTOP-8G69KAN\root <nkc17.yuya.hayase@gmail.com>
2018-06-27 17:35:14 +0900

    Scene

--- ShadowMove/Assets/Script/Hayase/hTutrialCamera.cs
+++ ShadowMove/Assets/Script/Hayase/hTutrialCamera.cs
@@ -11,7 +11,7 @@ public class hTutrialCamera : MonoBehaviour {
 	void Start () {
         if (player == null) player = GameObject.Find("TutrialPlayer");
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
         transform.position = player.transform.position + new Vector3(0,0,-10);
