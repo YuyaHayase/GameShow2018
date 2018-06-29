313d9b0ee74ecdc54ba9d0bc57a2f423323e7afe
fujiyama-pozi <nkc.17.hiroki.fujiyama@gmail.com>
2018-06-29 10:02:04 +0900

    とりあえず終わり

---
+++ ShadowMove/Assets/Script/fujiyama/scripts/Fhp.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Fhp : MonoBehaviour {
+    public Slider sllidre;
+
+    fShodow fs;
+	// Use this for initialization
+	void Start ()
+    {
+        fs = GameObject.Find("shodow").GetComponent<fShodow>();
+        sllidre.maxValue = fs.HP;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        sllidre.value = 1;
+	}
+}
